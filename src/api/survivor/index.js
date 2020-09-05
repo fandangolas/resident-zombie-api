a3b28d5a71a4884728526772e0a2ef532722dda7
Nícolas <nsilveira@braspag.com.br>
2020-09-05 00:13:16 -0300

    using locations repository to create a survivor

--- src/api/survivor/index.js
+++ src/api/survivor/index.js
@@ -1,20 +1,17 @@
 import Survivor from "../../domain/survivor/survivor";
 import { v4 as uuidv4 } from 'uuid';
 
-const survivorModule = ({ survivorsRepository }) => {
+const survivorModule = ({ survivorsRepository, locationsRepository }) => {
   const create = async ({ body }) => {
     try {
       const entity = Survivor(body);
+      const { lastLocation, items, ...survivor } = entity;
 
-      const { lastLocation, items, ...survivorProps } = entity;
+      const locationId = uuidv4();
 
-      const locationId = "8d5c03ce-d49e-407f-926c-066946748761";
+      await locationsRepository.create({ id: locationId , ...lastLocation });
 
-      const location = { id: locationId , ...lastLocation };
-
-      const survivor = { ...survivorProps, lastLocation: locationId };
-
-      const data = await survivorsRepository.create(survivor);
+      await survivorsRepository.create({ ...survivor, lastLocation: locationId });
     }
     
     catch (error) {
