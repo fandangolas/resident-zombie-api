6903304f386305fb8f90ffe9988a9ed4f877a50a
Nícolas <nsilveira@braspag.com.br>
2020-09-06 23:37:33 -0300

    adding more unit tests do survivor's module

--- tests/api/survivor/survivor.test.js
+++ tests/api/survivor/survivor.test.js
@@ -8,11 +8,12 @@ import SequelizeMock from "sequelize-mock";
 
 
 import { expect } from "chai";
-import sinon from "sinon";
+import sinon, { spy } from "sinon";
 
 
 
 describe("Survivor module testing", async function () {
+  //mock survivor module dependencies
   var dbMock = new SequelizeMock();
 
   const locationsRepositorySubstitute = locationsRepository({ db: dbMock });
@@ -49,7 +50,10 @@ describe("Survivor module testing", async function () {
   };
   const { lastLocation, items, ...survivor } = testSurvivor;
 
-  it('should find or create a new location when creating a survivor', async function () {
+  afterEach(() => sinon.restore());
+
+  it('should find or create a new location when registering a survivor on database',
+    async function () {
     // Arrange
     const spy = sinon.spy(locationsRepositorySubstitute, "findOrCreate");
 
@@ -59,5 +63,89 @@ describe("Survivor module testing", async function () {
     // Assert
     expect(spy.calledOnce).to.be.true;
     expect(spy.calledWithMatch(lastLocation, sinon.match.any)).to.be.true;
+  });
+
+  it('should register the new survivor to database', async function () {
+    // Arrange
+    const spy = sinon.spy(survivorsRepositorySubstitute, "create");
+    sinon
+      .stub(locationsRepositorySubstitute, "findOrCreate")
+      .returns({
+        id: "c96a0c15-6582-466f-ba2b-c324ff597adc",
+        ...lastLocation
+      });
+
+    // Act
+    await sut.create({ body: testSurvivor });
+
+    // Assert
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWithMatch({
+      ...survivor,
+      lastLocation: "c96a0c15-6582-466f-ba2b-c324ff597adc"
+    }, sinon.match.any)).to.be.true;
+  });
+
+  it(`should register survivor's items on database`, async function () {
+    // Arrange
+    const spy = sinon.spy(
+                          survivorItemsRepositorySubstitute,
+                          "createItemsForGivenSurvivor"
+                        );
+    sinon
+      .stub(locationsRepositorySubstitute, "findOrCreate")
+      .returns({
+        id: "c96a0c15-6582-466f-ba2b-c324ff597adc",
+        ...lastLocation
+      });
+    sinon
+      .stub(survivorsRepositorySubstitute, "create")
+      .returns({
+        id: "71d60368-e961-4f91-a35f-a47a90219e20",
+        ...survivor,
+        lastLocation: "c96a0c15-6582-466f-ba2b-c324ff597adc"
+      });
+
+    // Act
+    await sut.create({ body: testSurvivor });
+
+    // Assert
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith({
+      survivorId: sinon.match("71d60368-e961-4f91-a35f-a47a90219e20"),
+      items: [
+        sinon.match({ id: "37652173-72cd-494e-889a-5a0f6d8ce3bf", amount: 3 }),
+        sinon.match({ id: "faf8e188-abf2-444b-bdfe-6f2baba6b481", amount: 2 })
+      ]}, sinon.match.any)).to.be.true;
+  });
+
+  it(`should correctly register a new survivor`, async function () {
+    // Arrange
+    sinon
+      .stub(locationsRepositorySubstitute, "findOrCreate")
+      .returns({
+        id: "c96a0c15-6582-466f-ba2b-c324ff597adc",
+        ...lastLocation
+      });
+    sinon
+      .stub(survivorsRepositorySubstitute, "create")
+      .returns({
+        id: "71d60368-e961-4f91-a35f-a47a90219e20",
+        ...survivor,
+        lastLocation: "c96a0c15-6582-466f-ba2b-c324ff597adc"
+      });
+
+    const expected = {
+      id: "71d60368-e961-4f91-a35f-a47a90219e20",
+      ...survivor,
+      lastLocation: "c96a0c15-6582-466f-ba2b-c324ff597adc",
+      items
+    };
+
+    // Act
+    const actual = await sut.create({ body: testSurvivor });
+
+    // Assert
+    expect(actual).to.be.deep.equal(expected);
   });
 });
